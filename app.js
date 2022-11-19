6495d3e8b7dda2603b1fd24f8a28048ed8fccb84
Your Name <you@example.com>
2022-11-19 15:04:01 +0300

    close to final

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
+import userRoute from "./routes/userRoute.js";
+import { checkUser } from "./middlewares/authMiddleware.js";
+import fileUpload from "express-fileupload";
+import { v2 as cluodinary } from "cloudinary";
 
-const app = express();
-const port = 3000;
+dotenv.config();
 
-
-app.get('/', (req,res) => {
-    res.send('INDEX SAYFASI2');
+cluodinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
+//connection to the DB
+conn();
+
+const app = express();
+const port = process.env.PORT;
+
+//ejs template engine
+app.set("view engine", "ejs");
+
+//static files middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true }));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+//router
+app.use("*", checkUser);
+app.use("/", pageRoute);
+app.use("/photos", photoRoute);
+app.use("/users", userRoute);
+
 app.listen(port, () => {
-    console.log(`App running on port: ${port}`);
+  console.log(`App running on port: ${port}`);
 });
